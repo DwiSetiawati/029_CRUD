efd1dcb1ae987928393b7cd4a7a306b5fb6ec27b
DwiSetiawati <dwisetwa27@gmail.com>
2026-07-15 14:52:40 +0700

    inisialisasi server dasar express dan port listening

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const express = require('express');
-const {Pool} = require('pg');
 const app = express();
 const port = 3000;
-
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'mahasiswa',
-    password: 'Dwisetwa27',
-    port: 5432,
-});
-
-app.use(express.json());
-
 
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
